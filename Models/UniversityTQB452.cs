0c36b9783f4b7baa49d8f97e1f43ae21011a2e8e
tranquocbaoksnb <tranquocbaoksnb@gmail.com>
2021-12-17 15:47:23 +0700

    Create_Table_University_TQB452

--- Models/UniversityTQB452.cs
+++ Models/UniversityTQB452.cs
@@ -7,8 +7,10 @@ namespace TranQuocBao452.Models
     {
         [Key]
         [Display(Name = "ID trường")]
+        [column(TypeName = "nvarchar(50)”)]  
         public int PersonId { get; set; }
         [Display(Name = "tên trường")]
+        [column(TypeName = "nvarchar(50)”)]  
          public int Personname { get; set; }
        
     }
